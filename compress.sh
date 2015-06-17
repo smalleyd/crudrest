71d9077c1121e192a2766c6f6d181f54bbd4f6d9
David Small <smalleyd@Davids-MacBook-Pro.local>
2015-06-17 15:53:56 -0400

    Added comment about the Closure JS Compiler.

--- compress.sh
+++ compress.sh
@@ -1,1 +1,3 @@
+# https://developers.google.com/closure/compiler/?csw=1
+
 java -jar lib/compiler.jar --js_output_file=scripts/common.js scripts/common/template.js scripts/common/tabTemplate.js scripts/common/listTemplate.js scripts/common/editTemplate.js scripts/common/viewTemplate.js scripts/common/progressBar.js scripts/common/keyCodes.js scripts/common/dropdown.js scripts/common/datePicker.js scripts/common/login.js scripts/common/tagger.js scripts/common/timePicker.js scripts/common/timestampPicker.js
